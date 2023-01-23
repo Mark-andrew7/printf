875672a2b582f0fcf43cee08c28132c8fa6f1b4a
Mark-andrew7 <kamaumark19@gmail.com>
2023-01-23 05:25:49 -0800

    fprintf

---
+++ format.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _printf - produces output
+ * @format: specifier
+ * Return: output
+ */
+int _printf(const char *format, ...)
+{
+char *format;
+format = "alphabet";
+
+fprintf("%c" comes first in the "%s", "a", "format");
+}
