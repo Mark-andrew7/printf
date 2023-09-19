c4aec06cde3347cb60fa05ab0036ee2f0f7b026e
Mark-andrew7 <kamaumark19@gmail.com>
2023-09-19 06:15:18 -0700

    Fixed format specifiers

--- format_handlers.c
+++ format_handlers.c
@@ -6,24 +6,20 @@
  * @args: va_list containing the arguments
  * Return: number of characters printed for this format
  */
-int parse_format(const char *format, va_list args)
+int parse_format(const char *format, __attribute__((unused)) va_list args)
 {
 if (*format == 'd' || *format == 'i')
 {
-int num = va_arg(args, int);
-
 return (0);
 }
+
 else if (*format == 's')
 {
-char *str = va_arg(args, char *);
-
 return (0);
 }
+
 else if (*format == 'c')
 {
-int ch = va_arg(args, int);
-
 return (0);
 }
 
